b39de79ffad00450954d5a1a10130984cb8b6f13
Fredy Guzman <fredy.guzman@itksquare.edu.mx>
2022-11-28 10:53:40 -0600

    Finished Homework

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_class/controllers/theme_mode_provider.dart';
 import 'package:provider_class/screens/home.dart';
 
-//HERE YOU WILL SETUP YOU MILTIPROVIDER FUNCTION
-//DONT FORGET TO ADD YOUR PROVIDER CLASSES
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,6 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Material App', home: HomeScreen());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ThemeModeProvider(),
+        )
+      ],
+      child: MaterialApp(title: 'Material App', home: HomeScreen()),
+    );
   }
 }
